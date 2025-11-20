aef23103e2ffc887335cc2e891a95a7781a633c5
6631501044-droid <6631501044@lamduan.mfu.ac.th>
2025-11-20 20:16:34 +0700

    Disable

--- app.js
+++ app.js
@@ -8,6 +8,51 @@ app.use(express.json());
 
 
 
+// -------------------- DISABLE ROOM --------------------
+// Staff: Disable room (only when room is ENABLE)
+app.put("/disable-room/:room_id", (req, res) => {
+    const room_id = req.params.room_id;
+
+   
+    const checkSql = `
+        SELECT room_status 
+        FROM room 
+        WHERE room_id = ?
+    `;
+
+    db.query(checkSql, [room_id], (err, result) => {
+        if (err) return res.status(500).json({ error: err });
+
+        if (result.length === 0)
+            return res.status(404).json({ message: "Room not found" });
+
+        const status = result[0].room_status;
+
+
+        if (status !== "enable") {
+            return res.status(400).json({
+                message: "Room cannot be disabled. Only rooms with 'enable' status can be disabled."
+            });
+        }
+
+        
+        const disableSql = `
+            UPDATE room 
+            SET room_status = 'disable'
+            WHERE room_id = ?
+        `;
+
+        db.query(disableSql, [room_id], (err2, result2) => {
+            if (err2) return res.status(500).json({ error: err2 });
+
+            return res.json({
+                message: "Room disabled successfully",
+                room_id: room_id
+            });
+        });
+    });
+});
+
 
 //=================== Starting server =======================
 const port = 3000;
